6ce36349be8cad50e709ada862574982eeaaab5f
mrs.sakshi <133587092+sakshikharchan@users.noreply.github.com>
2024-12-02 06:35:55 +0000

    strict comparison check

---
+++ 01_Basic/Comparison.js
@@ -0,0 +1,11 @@
+console.log("2">1)
+console.log(2!=1)
+console.log(null==0)
+console.log(null>0)
+console.group(null<0)
+console.log(null>=0)
+console.log(undefined>=0)
+
+
+//=== this is strictly check what datatype should be we taken
+console.log("2"===2)
