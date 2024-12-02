1a410e852be21154e8de0d84faa06fa26bac29d3
mrs.sakshi <133587092+sakshikharchan@users.noreply.github.com>
2024-12-02 06:01:46 +0000

    conversion of data types

---
+++ 01_Basic/conversion_operation.js
@@ -0,0 +1,29 @@
+/*let score =33
+console.log(typeof(score))
+console.log(typeof score)
+
+// suppose i am give the value in string formst means in "" then what result be show
+*/
+let scoremarks =undefined
+//console.log(typeof scoremarks)
+
+//Number  conversion
+let valueinnumber =Number(scoremarks)
+//console.log(valueinnumber)
+
+
+/*
+true 1 
+false 0
+"123abc" Nan
+
+*/
+//string conversion
+let somenumber =33
+let stringinnum =String(somenumber)
+//console.log(typeof stringinnum)
+
+//boolean conversion
+let a= 1
+let booleanin=Boolean(a)
+console.log(booleanin)
