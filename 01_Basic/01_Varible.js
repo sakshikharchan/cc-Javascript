a49bd682ee378f295f8ad9b10ae3a2bc7b131c10
mrs.sakshi <133587092+sakshikharchan@users.noreply.github.com>
2024-12-02 05:10:04 +0000

    info varible

---
+++ 01_Basic/01_Varible.js
@@ -0,0 +1,19 @@
+const accountId = 128742
+let accountEmail = "sakshikharchan88@gamil.com"
+var accountpassword = "12234"
+accountCity = "jaipur"
+accountEmail = "sam@gmail.com"
+let accountstate
+//accountId=2
+console.log(accountId);
+
+console.log(accountEmail);
+console.log(accountpassword);
+console.table([accountEmail, accountId, accountpassword, accountCity,accountstate])
+/*
+prefer not to use var because var are not alloacted  block scope
+Example:
+{
+}
+*/
+
