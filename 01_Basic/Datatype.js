efac5daeb02dd6f02a480ef67840f362a8a26bf8
mrs.sakshi <133587092+sakshikharchan@users.noreply.github.com>
2024-12-02 05:28:38 +0000

    Datatypes

---
+++ 01_Basic/Datatype.js
@@ -0,0 +1,20 @@
+"use strict"; // treats all js code as newer version
+console.log(3 + 3);
+console.log("sam");
+let name = "hitesh";
+let age = 19;
+let isloggin = true;
+
+/*number => 2  to power 53
+bigint
+string =""
+boolean =true/false
+null =>standaone value
+undefined=
+symbol =unique
+*/
+console.log(typeof undefined);
+console.log(typeof Number);
+console.log(typeof Boolean);
+console.log(typeof "hites");
+console.log(typeof null);
